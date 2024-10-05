1b8ca0978e05571cc3e54a6c191825c942df1ce3
Yaosanqi137 <zxc18023571263@outlook.com>
2024-10-05 20:08:15 +0800

    Uno plugin has been built

---
+++ src/main/java/gstech/yao37/uno.java
@@ -0,0 +1,17 @@
+package gstech.yao37;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class uno extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
